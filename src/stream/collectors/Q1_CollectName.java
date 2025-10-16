864fe163fa411fa5fa5193013de59225225a6bc1
Suryoday Yadav <133846974+SuryodayYadav@users.noreply.github.com>
2025-10-16 21:26:38 +0530

    practicing collectors - phase 1

--- src/stream/collectors/Q1_CollectName.java
+++ src/stream/collectors/Q1_CollectName.java
@@ -1,7 +1,7 @@
 package stream.collectors;
 
-import java.util.List;
-import java.util.Set;
+import java.net.CookieHandler;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Q1_CollectName {
@@ -35,10 +35,83 @@ public class Q1_CollectName {
     }
     //here to start:
     public static void maxBy(List<Employee> list){
-        list.stream().collect(Collectors.averagingInt(x -> x.getAge()));
-        Double sumage = list.stream().collect(Collectors.averagingInt(Employee::getAge));
-        System.out.println(sumage);
+       Employee maxSalary =  list.stream().collect(Collectors.maxBy(Comparator.comparing(x -> x.getSalary())))
+               .orElse(null);
+        Employee maxSalaryName =  list.stream().max(Comparator.comparing(Employee::getSalary))
+                .orElse(null);
+        assert maxSalary != null;
+        System.out.println(maxSalary.getName());
     }
+    public static void youngestOne(List<Employee> list){
+        Employee age =  list.stream().collect(Collectors.minBy(Comparator.comparing(x -> x.getAge())))
+                .orElse(null);
+        Optional<Employee> youngest =  list.stream().min(Comparator.comparing(Employee::getAge));
+        assert age != null;
+        System.out.println(youngest);
+        System.out.println(age.getName());
+    }
+    public static void groupByDept(List<Employee> list){
+         Map<String, List<String>> map = list.stream().collect(Collectors.groupingBy(x -> x.getDepartment(),
+                Collectors.mapping(x -> x.getName(), Collectors.toList())));
+        System.out.println(map);
+    }
+    public static void countByDept(List<Employee> list){
+        Map<String, Long> map = list.stream().collect(Collectors.groupingBy(Employee::getDepartment,
+                Collectors.counting()));
+        System.out.println(map);
+    }
+    public static void salaryByDept(List<Employee> list){
+        Map<String, Double> map = list.stream().collect(Collectors.groupingBy(Employee::getDepartment,
+                Collectors.summingDouble(x -> x.getSalary())));
+        System.out.println(map);
+    }
+    public static void averageSalaryByDept(List<Employee> list){
+        Map<String, Double> map = list.stream().collect(Collectors.groupingBy(Employee::getDepartment,
+                Collectors.averagingDouble(x -> x.getSalary())));
+        System.out.println(map);
+    }
+    public static void groupByCity(List<Employee> list){
+        Map<String, List<String>> map = list.stream().collect(Collectors.groupingBy(x -> x.getCity(),
+                Collectors.mapping(x -> x.getName(), Collectors.toList())
+                ));
+        System.out.println(map);
+    }
+    public static void highestPaid(List<Employee> list){
+        Map<String, Optional<Employee>> map = list.stream().collect(Collectors.groupingBy(x -> x.getDepartment(),
+                Collectors.maxBy(Comparator.comparingDouble(x -> x.getSalary()))
+                ));
+        System.out.println(map);
+    }
+    public static void parttioningEmployee(List<Employee> list){
+       Map<Boolean, List<Employee>> map = list.stream().collect(Collectors.partitioningBy(x -> x.isActive()));
+        System.out.println(map);
+    }
+    public static void activeEmployee(List<Employee> list){
+        Map<Boolean, Long> map = list.stream().collect(Collectors.partitioningBy(x -> x.isActive(), Collectors.counting()));
+        System.out.println(map);
+    }
+    public static void highestSalary(List<Employee> list){
+          String name = list.stream().collect(Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(
+                x -> x.getSalary())),
+                e -> e.map(x -> x.getName()).orElse(null)
+        ));
+        System.out.println(name);
+    }
+    public static void highestPaidEmpName(List<Employee> list){
+        Map<String, Object> map = list.stream().collect(Collectors.groupingBy(x -> x.getDepartment(),
+               Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(x -> x.getSalary())),
+               opt -> opt.map(x -> x.getName()).orElse(null)
+               )
+        ));
+        System.out.println(map);
+    }
+    public static void groupingByDeptCity(List<Employee> list){
+         Map<String, Map<String, List<Employee>>> map = list.stream().collect(Collectors.groupingBy(x -> x.getDepartment(),
+                Collectors.groupingBy(x -> x.getCity())
+                ));
+        System.out.println(map);
+    }
+
 
     public static void main(String[] args) {
         List<Employee> list = DataFactory.getEmployees();
@@ -48,5 +121,18 @@ public class Q1_CollectName {
         collectEmployeeIT(list);
         collectSalaryHR(list);
         avgAge(list);
+        maxBy(list);
+        youngestOne(list);
+        groupByDept(list);
+        countByDept(list);
+        salaryByDept(list);
+        averageSalaryByDept(list);
+        groupByCity(list);
+        highestPaid(list);
+        parttioningEmployee(list);
+        activeEmployee(list);
+        highestSalary(list);
+        highestPaidEmpName(list);
+        groupingByDeptCity(list);
     }
 }
